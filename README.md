c9590041391b1572ce78868c90f57a15ba9aca23
Brian Heidrich <brian.heidrich@icloud.com>
2020-09-25 22:18:50 -0500

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# grss
+
+## Introduction
+
+> Simple project to help me learn and understand the Rust programming language
+>https://rust-cli.github.io/book/index.html
+
+## Code Samples
+
+> cargo run -- some_text some_file.txt
+
+## Installation
+
+> Clone the repo and run with the code above!
---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::{thread, time};
+use structopt::StructOpt;
+use anyhow::{Context, Result};
+
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+#[derive(Debug)]
+struct CustomError(String);
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
+}
